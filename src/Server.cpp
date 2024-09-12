36d5bdcd8c342e3f56cab46524e2642371525c6b
Sam <rsaumya2001@gmail.com>
2024-09-13 01:35:27 +0530

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,10 +3,13 @@
 #include <algorithm>
 
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
+    std::string alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFIGHKLMNOPQRSTUVWXYZ0123456789_";
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
-    }else if(pattern == "\\d"){
+    }else if (pattern == "\\d"){
         return input_line.find_first_of("0123456789") != std::string::npos;
+    }else if (pattern == "\\w"){
+        return input_line.find_first_of(alphaNumeric) != std::string::npos;
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
