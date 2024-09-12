9290f878a094ba6e33f55b32b87818e3896f527b
Sam <rsaumya2001@gmail.com>
2024-09-13 01:20:02 +0530

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
-#include <algorithm?
+#include <algorithm>
 
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
-    }else if(pattern == "\\d")
-        return input_line.find_first_of('0123456789') != std::string::npos;
+    }else if(pattern == "\\d"){
+        return input_line.find_first_of("0123456789") != std::string::npos;
+    }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
     }
